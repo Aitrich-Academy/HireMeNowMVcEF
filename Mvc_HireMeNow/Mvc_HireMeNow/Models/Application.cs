c9dc474b32b386157e945726429a33f39f4ea5b5
Anuvind <soudha.aitrich@gmail.com>
2023-08-04 14:45:51 +0530

    commit

--- Mvc_HireMeNow/Mvc_HireMeNow/Models/Application.cs
+++ Mvc_HireMeNow/Mvc_HireMeNow/Models/Application.cs
@@ -1,30 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-
-namespace Mvc_HireMeNow.Models;
-
-public partial class Application
+﻿namespace Mvc_HireMeNow.Models
 {
-	[Key]
-	[Required]
-	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-	public Guid Id { get; set; }
-	[ForeignKey(nameof(User))]
-	public Guid? UserId { get; set; }
-	[ForeignKey(nameof(Job))]
-	public Guid? JobId { get; set; }
+	public class Application
+	{
 
-	[ForeignKey(nameof(Company))]
-	public Guid? CompanyId { get; set; }
-
-	public DateTime? AppliedDate { get; set; }
-
-    public string? Status { get; set; }
-
-    public virtual Job? Job { get; set; }
-
-    public virtual User? User { get; set; }
-	public virtual Company? Company { get; set; }
+	}
 }
