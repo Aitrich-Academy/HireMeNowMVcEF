af318fb2b8b34bc0affc07f03f3a0087a7c44a70
RaziyaShajeer <rasiyaaitrich@gmail.com>
2023-08-09 17:04:45 +0530

    Commit

--- Mvc_HireMeNow/Mvc_HireMeNow/Controllers/JobProviderController.cs
+++ Mvc_HireMeNow/Mvc_HireMeNow/Controllers/JobProviderController.cs
@@ -3,18 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 using Mvc_HireMeNow.Dtos;
 using Mvc_HireMeNow.Interfaces;
 using Mvc_HireMeNow.Models;
+using System.Threading.Tasks.Dataflow;
 
 namespace Mvc_HireMeNow.Controllers
 {
 	public class JobProviderController : Controller
 	{
 		IMapper _mapper;
-		IJobRepository JobRepository;
+		IJobRepository _jobRepository;
+		IUserRepository _userRepository;
 
-		public JobProviderController(IMapper mapper, IJobRepository jobRepository)
+		public JobProviderController(IMapper mapper, IJobRepository jobRepository,IUserRepository userRepository)
 		{
 			_mapper = mapper;
-			JobRepository = jobRepository;
+			_jobRepository = jobRepository;
+			_userRepository = userRepository;
+
 		}
 
 		public IActionResult Index()
@@ -29,9 +33,14 @@ namespace Mvc_HireMeNow.Controllers
 		[ValidateAntiForgeryToken]
 		public IActionResult PostJob(JobDto jobDto)
 		{
-			jobDto.CompanyId = new Guid("95541DEF-A31C-4BEA-9472-6B103C708487");
+			var uid = HttpContext.Session.GetString("UserId");
+
+
+			
+			User user = _userRepository.getById(new Guid(uid));
+			jobDto.CompanyId = (Guid)user.CompanyId;
 			var job = _mapper.Map<Job>(jobDto);
-			bool result=JobRepository.Create(job);
+			bool result=_jobRepository.Create(job);
 		
 			
 				TempData["Message"] = "Successfully posted Job";
